aa2a6a6614fa99da0712fd1833d98784a5eb53c5
Navindra001 <komatinavindra@gmail.com>
2026-07-14 03:24:36 +0100

    fix CORS in production settings

--- backend/core/settings/production.py
+++ backend/core/settings/production.py
@@ -1,4 +1,5 @@
 from .base import *  # noqa
+import os
 
 DEBUG = False
 SECURE_SSL_REDIRECT = True
@@ -7,3 +8,9 @@ SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+
+CORS_ALLOWED_ORIGINS = [
+    "https://vagus-ai-s9s4.vercel.app",
+    "https://vagus-ai-s9s4-git-main-navindra001s-projects.vercel.app",
+]
+CORS_ALLOW_CREDENTIALS = True
